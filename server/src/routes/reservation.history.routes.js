90ebf8ed3735f95ee54d8ce9a1f0aed882dc586d
Mushfique Nayeeb Ayon <musfiqueayon01@gmail.com>
2026-09-03 21:57:22 +0600

    feat(F04): Reservation History

--- server/src/routes/reservation.history.routes.js
+++
@@ -1,9 +0,0 @@
-﻿// F04 Reservation History Routes
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/history', getReservationHistory);
-router.get('/history/:id', getReservationById);
-
-module.exports = router;
